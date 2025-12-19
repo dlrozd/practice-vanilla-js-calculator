f3ba12ec2f1613713ea9a2831b25e5fa85c5f166
drozd <mayakovsky03@gmail.com>
2025-12-19 17:52:05 +0200

    refactoring code for better organization and readability

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
+// data
+
 const buttonAddition = document.getElementById( 'buttonAddition');
 const buttonSubstraction = document.getElementById( 'buttonSubstraction');
 const buttonMultiply = document.getElementById( 'buttonMultiplication');
 const buttonDivision = document.getElementById( 'buttonDivision');
-// const calculateBtn = document.getElementById('calculate');
-// const resultDisplay = document.getElementById('result');
 
-// let selectedOperation = null;
+let inputNumber1 = document.getElementById('input-number-1');
+let inputNumber2 = document.getElementById('input-number-2');
 
-// function setAddition() { selectedOperation = '+'; }
-// function setSubtraction() { selectedOperation = '-'; }
-// function setMultiply() { selectedOperation = '*'; }
-// function setDivision() { selectedOperation = '/'; }
+const operationButtons = [buttonAddition, buttonSubstraction, buttonMultiply, buttonDivision]
 
-function buttonAdditionClick () {
-    let inputNumber1 = document.getElementById('input-number-1');
-    let inputNumber2 = document.getElementById('input-number-2');
+// logic
+
+function makeOperation (operation) {
     let number1 = Number(inputNumber1.value);
     let number2 = Number(inputNumber2.value);
-    return alert(number1 + number2);
-}
-function buttonSubtractionClick () {
-    let inputNumber1 = document.getElementById('input-number-1');
-    let inputNumber2 = document.getElementById('input-number-2');
-    let number1 = Number(inputNumber1.value);
-    let number2 = Number(inputNumber2.value);
-    return alert(number1 - number2);
+    let result = null;
 
-}
-function buttonMultiplyClick () {
-    let inputNumber1 = document.getElementById('input-number-1');
-    let inputNumber2 = document.getElementById('input-number-2');
-    let number1 = Number(inputNumber1.value);
-    let number2 = Number(inputNumber2.value);
-    return alert(number1 * number2);
-}
-function buttonDivisionClick () {
-    let inputNumber1 = document.getElementById('input-number-1');
-    let inputNumber2 = document.getElementById('input-number-2');
-    let number1 = Number(inputNumber1.value);
-    let number2 = Number(inputNumber2.value);
-    return alert(number1 / number2);
+    if (operation === '+') {
+        result = number1 + number2;
+    } else if (operation === '-') {
+        result = number1 - number2;
+    } else if (operation === '*') {
+        result = number1 * number2;
+    } else if (operation === '/') {
+        result = number1 / number2;
+    } else {
+        return alert('Please select an operation');
+    }
+
+    return alert(result);
 }
 
+function buttonOperationClick (eventObject) {
+    let clickedElement = eventObject.currentTarget;
+    let operation = clickedElement.innerHTML;
+    makeOperation(operation);
+}
 
+for (let i = 0; i < operationButtons.length; i++) {
+    operationButtons[i].addEventListener('click', buttonOperationClick);
+}
 
-buttonAddition.addEventListener('click', buttonAdditionClick);
-buttonSubstraction.addEventListener('click', buttonSubtractionClick);
-buttonMultiply.addEventListener('click', buttonMultiplyClick);
-buttonDivision.addEventListener('click', buttonDivisionClick);
 
 
